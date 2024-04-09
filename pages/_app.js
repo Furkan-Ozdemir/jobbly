b294624011538aa5f53779960ccafbdec8f0f8b8
Furkan Özdemir <ozthefur@gmail.com>
2024-04-10 00:09:05 +0300

    Add meta tags and title to improve SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "@/styles/globals.css";
 import { ibmPlexSansHebrew } from "@/utils/fonts";
 import Layout from "@/components/Layout";
@@ -5,6 +6,28 @@ import Layout from "@/components/Layout";
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Jobbly</title>
+        <meta
+          name="description"
+          content="Jobbly is an internship/new grad role finder app."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="tags"
+          content="internship, job, new grad, jobbly, software engineering"
+        />
+        <meta
+          name="author"
+          content="Furkan Özdemir, linkedin.com/in/furkan-o-demir/"
+        />
+        <meta property="og:title" content="Jobbly" />
+        <meta
+          property="og:description"
+          content="Jobbly is an internship/new grad role finder app."
+        />
+        <meta property="og:type" content="website" />
+      </Head>
       <style jsx global>{`
         :root {
           --ibm-sans-hebrew-font: ${ibmPlexSansHebrew.style.fontFamily};
