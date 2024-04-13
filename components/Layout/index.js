6396f1b4605d4c034529dcf211cb6b499a871b70
Furkan Özdemir <ozthefur@gmail.com>
2024-04-13 22:24:44 +0300

    Page transition improved

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -3,11 +3,24 @@ import { Fragment, useEffect } from "react";
 import Navbar from "@/components/Navbar";
 import { animatePageIn } from "@/utils/animations";
 import generateColor from "@/utils/generateColor";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { TextPlugin } from "gsap/dist/TextPlugin";
+
+gsap.registerPlugin(useGSAP, TextPlugin);
 
 export default function Layout(props) {
   const color = generateColor();
   useEffect(() => {
     animatePageIn();
+  });
+  useGSAP(() => {
+    gsap.to(`#loading`, {
+      text: "loading...",
+      duration: 1,
+      repeat: -1,
+      yoyo: true,
+    });
   });
   return (
     <Fragment>
@@ -15,10 +28,11 @@ export default function Layout(props) {
         style={{
           height: "100vh",
           width: "25vw",
-          background: `${color}`,
+          background: `${color.background}`,
           position: "fixed",
           zIndex: 10,
           left: 0,
+          color: `${color.text}`,
         }}
         id="bannerOne"
       ></div>
@@ -26,23 +40,31 @@ export default function Layout(props) {
         style={{
           height: "100vh",
           width: "25vw",
-          background: `${color}`,
+          background: `${color.background}`,
           position: "fixed",
           zIndex: 10,
           left: "25%",
           filter: "brightness(0.9)",
+          color: `${color.text}`,
+          fontSize: "4rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
         id="bannerTwo"
-      ></div>
+      >
+        <div id="loading"></div>
+      </div>
       <div
         style={{
           height: "100vh",
           width: "25vw",
-          background: `${color}`,
+          background: `${color.background}`,
           position: "fixed",
           zIndex: 10,
           left: "50%",
           filter: "brightness(0.8)",
+          color: `${color.text}`,
         }}
         id="bannerThree"
       ></div>
@@ -50,11 +72,12 @@ export default function Layout(props) {
         style={{
           height: "100vh",
           width: "25vw",
-          background: `${color}`,
+          background: `${color.background}`,
           position: "fixed",
           zIndex: 10,
           left: "75%",
           filter: "brightness(0.7)",
+          color: `${color.text}`,
         }}
         id="bannerFour"
       ></div>
--- utils/generateColor.js
+++ utils/generateColor.js
@@ -1,15 +1,15 @@
 const COLORS = [
-  "#193053",
-  "#17A980",
-  "#87C33D",
-  "#FEABA9",
-  "#FDF4CD",
-  "#F6A48C",
-  "#727EB5",
-  "#533E59",
-  "#2F2345",
-  "#029AD6",
-  "#c4cf4a",
+  { background: "#193053", text: "#E6CFAC" },
+  { background: "#17A980", text: "#E8607F" },
+  { background: "#87C33D", text: "#783C68" },
+  { background: "#FEABA9", text: "#65549C" },
+  { background: "#FDF4CD", text: "#660F96" },
+  { background: "#F6A48C", text: "#6D5B4B" },
+  { background: "#727EB5", text: "#8D8B4A" },
+  { background: "#533E59", text: "#ACC1A6" },
+  { background: "#2F2345", text: "#D0DEBE" },
+  { background: "#029AD6", text: "#FD3C66" },
+  { background: "#c4cf4a", text: "#453ABA" },
 ];
 
 export default function generateColor() {
